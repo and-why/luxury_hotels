7039196677584ccacaad0165362f57c7577f8736
Andy Smith <work@andysmith.is>
2021-07-20 16:31:42 +1000

    fixed build bugs

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-import { Heading, Image, Text } from '@chakra-ui/react';
 import Layout from '@/components/Layout';
-import { getHotels, getInitialHotels } from '@/utils/hotels';
-import { getToken } from '@/utils/token';
 import SearchBigHeader from '@/components/SearchBigHeader';
-import { useState } from 'react';
 
 export default function Home(props) {
   // const [data, setData] = useState(props.hotels.data);
@@ -16,9 +12,3 @@ export default function Home(props) {
     </>
   );
 }
-
-// export async function getStaticProps() {
-//   const hotels = await getInitialHotels();
-
-//   return { props: { hotels }, revalidate: 1 };
-// }
