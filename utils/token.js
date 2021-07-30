37c5a77d0eeb1440d15d3f792f20da930b00b3ae
Andy Smith <work@andysmith.is>
2021-07-30 13:47:22 +1000

    production api

--- utils/token.js
+++ utils/token.js
@@ -3,7 +3,7 @@ import Amadeus from 'amadeus';
 export async function getToken() {
   const amadeus = await new Amadeus({
     // hostname: 'production',
-    // hostname: process.env.NODE_ENV !== 'development' && 'production',
+    hostname: 'production',
     clientId: process.env.NEXT_PUBLIC_AMADEUS_API_KEY,
     clientSecret: process.env.NEXT_PUBLIC_AMADEUS_API_SECRET,
   });
