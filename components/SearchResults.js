911eb5b2a8e33fb1d014eb657bc9ef6cda0c58d9
Andy Smith <andysmith@Andys-MacBook-Air.local>
2021-08-27 15:34:48 +1000

    changed illustration

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -20,14 +20,15 @@ export default function SearchResults({ data }) {
           w='100%'
           wrap='wrap'
           transition='all ease 0.5s'
+          env
         >
           {data ? (
             data.map((hotel, index) => {
-              if (index % 5 === 0 && index !== 0) {
+              if (index === Math.floor(Math.random() * 6)) {
                 return (
                   <Box padding={2} w={['50%', '50%', '33.3%', '25%']}>
                     <NextImage
-                      src={`/images/search/${Math.floor(Math.random() * 12)}.png`}
+                      src={`/images/search/${Math.floor(Math.random() * 15)}.png`}
                       height={250}
                       width={250}
                     />
