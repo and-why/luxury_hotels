1cfb908b530289aeb161d64648e5a8b73d97eaa7
Andy Smith <andysmith@Andys-MacBook-Air.local>
2021-09-03 10:03:55 +1000

    fixed bug

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,32 +1,20 @@
-import { Box, Flex, Heading, Button } from '@chakra-ui/layout';
+import { Box, Flex, Heading } from '@chakra-ui/layout';
 import NextImage from 'next/image';
-import { useState } from 'react';
 import Container from './Container';
 import DisplayTile from './DisplayTile';
-import HotelListItem from './HotelListItem';
 
 export default function SearchResults({ data }) {
   // console.log('SearchResults:', data);
-  const [listMode, setListMode] = useState(false);
 
-  const handleView = () => {
-    setListMode(!listMode);
-  };
+  if (!data) return <p>loading</p>;
 
-  if (!data) {
-    return (
-      <Heading as='h3' fontSize='xl' textAlign='center' w='100%'>
-        No results for those dates or location.
-      </Heading>
-    );
-  }
   return (
     <Container>
       <Flex direction='column' w='100%' justify='flex-start'>
         <Heading fontSize='2xl' p={4} textTransform='capitalize'>
           Search Results
         </Heading>
-        <Button onClick={handleView}>{listMode ? 'Grid' : 'List'}</Button>
+
         <Flex
           justify='flex-start'
           align='flex-start'
@@ -49,13 +37,15 @@ export default function SearchResults({ data }) {
               width='400px'
             />
           </Flex>
-          {!listMode
-            ? data.data.map((hotel, index) => {
-                return <DisplayTile key={index} data={hotel} dictionary={data.dictionaries} />;
-              })
-            : data.data.map((hotel, index) => {
-                return <HotelListItem key={index} data={hotel} dictionary={data.dictionaries} />;
-              })}
+          {data ? (
+            data.data.map((hotel, index) => {
+              return <DisplayTile key={index} data={hotel} dictionary={data.dictionaries} />;
+            })
+          ) : (
+            <Heading as='h3' fontSize='xl' textAlign='center' w='100%'>
+              No results for those dates or location.
+            </Heading>
+          )}
           <Flex
             direction='column'
             w={['50%', '50%', '33.3%', '25%']}
